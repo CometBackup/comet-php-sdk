b3d9457f48aac83d20862bd4673fd77c5f3804c6
Mason <mason@myclientglobal.com>
2018-09-21 11:44:55 +1200

    comet: add copyright header to all *.php files with SPDX identifer

--- Comet/U2FSignResponse.php
+++ Comet/U2FSignResponse.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * Copyright (c) 2018 Comet Licensing Ltd.
+ * Please see the LICENSE file for usage information.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
 namespace Comet;
 
 class U2FSignResponse {
