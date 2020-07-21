773de5625e727340578bd4d466db3afe60533dac
Mason <mason@cometbackup.com>
2020-07-21 16:01:17 +1200

    comet: support remotely browsing Hyper-V VMs

---
+++ Comet/AdminDispatcherRequestBrowseHypervRequest.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * Copyright (c) 2018-2020 Comet Licensing Ltd.
+ * Please see the LICENSE file for usage information.
+ * 
+ * SPDX-License-Identifier: MIT
+ */
+
+namespace Comet;
+
+/** 
+ * Comet Server AdminDispatcherRequestBrowseHyperv API 
+ * Request a list of Hyper-V virtual machines from a live connected device
+ * 
+ * You must supply administrator authentication credentials to use this API.
+ * This API requires the Auth Role to be enabled.
+ */
+class AdminDispatcherRequestBrowseHypervRequest implements \Comet\NetworkRequest {
+	
+	/**
+	 * The live connection GUID
+	 *
+	 * @var string
+	 */
+	protected $TargetID = null;
+	
+	/**
+	 * Construct a new AdminDispatcherRequestBrowseHypervRequest instance.
+	 *
+	 * @param string $TargetID The live connection GUID
+	 */
+	public function __construct($TargetID)
+	{
+		$this->TargetID = $TargetID;
+	}
+	
+	/**
+	 * Get the URL where this POST request should be submitted to.
+	 *
+	 * @return string
+	 */
+	public function Endpoint()
+	{
+		return '/api/v1/admin/dispatcher/request-browse-hyperv';
+	}
+	
+	public function Method()
+	{
+		return 'POST';
+	}
+	
+	/**
+	 * Get the POST parameters for this request.
+	 *
+	 * @return string[]
+	 */
+	public function Parameters()
+	{
+		$ret = [];
+		$ret["TargetID"] = (string)($this->TargetID);
+		return $ret;
+	}
+	
+	/**
+	 * Decode types used in a response to this request.
+	 * Use any network library to make the request.
+	 *
+	 * @param int $responseCode HTTP response code
+	 * @param string $body HTTP response body
+	 * @return \Comet\BrowseHVResponse 
+	 * @throws \Exception
+	 */
+	public static function ProcessResponse($responseCode, $body)
+	{
+		// Require expected HTTP 200 response
+		if ($responseCode !== 200) {
+			throw new \Exception("Unexpected HTTP " . intval($responseCode) . " response");
+		}
+		
+		// Decode JSON
+		$decoded = \json_decode($body); // as stdClass
+		if (\json_last_error() != \JSON_ERROR_NONE) {
+			throw new \Exception("JSON decode failed: " . \json_last_error_msg());
+		}
+		
+		// Try to parse as error format
+		$isCARMDerivedType = (($decoded instanceof \stdClass) && property_exists($decoded, 'Status') && property_exists($decoded, 'Message'));
+		if ($isCARMDerivedType) {
+			$carm = \Comet\APIResponseMessage::createFromStdclass($decoded);
+			if ($carm->Status !== 200) {
+				throw new \Exception("Error " . $carm->Status . ": " . $carm->Message);
+			}
+		}
+		
+		// Parse as BrowseHVResponse
+		if (is_array($decoded) && count($decoded) === 0) {
+		// Work around edge case in json_decode--json_encode stdClass conversion
+			$ret = \Comet\BrowseHVResponse::createFromStdclass(new \stdClass());
+		} else {
+			$ret = \Comet\BrowseHVResponse::createFromStdclass($decoded);
+		}
+		
+		return $ret;
+	}
+	
+}
+
--- Comet/Server.php
+++ Comet/Server.php
@@ -785,6 +785,23 @@ class Server {
 	}
 
 	/** 
+	 * Request a list of Hyper-V virtual machines from a live connected device
+	 * 
+	 * You must supply administrator authentication credentials to use this API.
+	 * This API requires the Auth Role to be enabled.
+	 *
+	 * @param string $TargetID The live connection GUID
+	 * @return \Comet\BrowseHVResponse 
+	 * @throws \Exception
+	 */
+	public function AdminDispatcherRequestBrowseHyperv($TargetID)
+	{
+		$nr = new \Comet\AdminDispatcherRequestBrowseHypervRequest($TargetID);
+		$response = $this->client->send($this->AsPSR7($nr));
+		return \Comet\AdminDispatcherRequestBrowseHypervRequest::ProcessResponse($response->getStatusCode(), (string)$response->getBody());
+	}
+
+	/** 
 	 * Request a list of installed VSS Writers (Application-Aware Writers) from a live connected device
 	 * 
 	 * You must supply administrator authentication credentials to use this API.
