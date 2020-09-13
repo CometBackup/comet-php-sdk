2f7cf5fa5fe51c4aa3d7743d01e1d88d0ed8e8c3
Rane Lafraze <lafrazeRL@gmail.com>
2020-09-13 17:38:22 -0400

    Fix formatting
    Formatting appeared to be misaligned on GitHub; correct these changes

--- Comet/Server.php
+++ Comet/Server.php
@@ -36,12 +36,12 @@ class Server {
 	 */
 	protected $password = '';
 
-    /**
+        /**
 	 * The TOTP code for administrative API requests to the Comet Server
 	 *
 	 * @var string
 	 */
-    protected $TOTPCode = '';
+        protected $TOTPCode = '';
 
 	/**
 	 * The GuzzleHttp client used to make synchronous network requests
@@ -85,15 +85,16 @@ class Server {
 		$this->client = $client;
 	}
 
-    /**
+	/**
 	 * Supply a TOTP code to authenticate 2FA-restricted accounts
 	 *
 	 * @param string $TOTPCode
 	 * @return void
 	 */
-	public function setTOTPCode($TOTPCode) {
+	public function setTOTPCode($TOTPCode)
+	{
 		$this->TOTPCode = $TOTPCode;
-    }
+	}
 
 	/** 
 	 * Retrieve properties about the current admin account
@@ -2204,15 +2205,14 @@ class Server {
 	 * @return \Psr\Http\Message\RequestInterface
 	 */
 	public function AsPSR7(NetworkRequest $nr, $TOTPCode = '') {
-
 		$params = $nr->Parameters();
 		$params['Username'] = $this->username;
 		$params['AuthType'] = 'Password';
-        $params['Password'] = $this->password;
-        if((strlen($TOTPCode) > 0) || (strlen($this->TOTPCode) > 0)) {
-            $params['AuthType'] = 'PasswordTOTP';
-            $params['TOTP'] = (strlen($TOTPCode) > 0 ? $TOTPCode : $this->TOTPCode);
-        }
+		$params['Password'] = $this->password;
+		if((strlen($TOTPCode) > 0) || (strlen($this->TOTPCode) > 0)) {
+			$params['AuthType'] = 'PasswordTOTP';
+			$params['TOTP'] = (strlen($TOTPCode) > 0 ? $TOTPCode : $this->TOTPCode);
+		}
 
 		return new \GuzzleHttp\Psr7\Request(
 			$nr->Method(),
