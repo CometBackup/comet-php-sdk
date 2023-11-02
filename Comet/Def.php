306920c7b3d3a1f11fe8d229d6152496a84c0b11
christiaan <christiaan@cometbackup.com>
2023-11-03 08:50:12 +1300

    comet: sync with comet 23.9.8

--- Comet/Def.php
+++ Comet/Def.php
@@ -14,7 +14,7 @@ class Def {
 	/**
 	 * @type string
 	 */
-	const APPLICATION_VERSION = "23.9.7";
+	const APPLICATION_VERSION = "23.9.8";
 
 	/**
 	 * @type int
@@ -29,7 +29,7 @@ class Def {
 	/**
 	 * @type int
 	 */
-	const APPLICATION_VERSION_REVISION = 7;
+	const APPLICATION_VERSION_REVISION = 8;
 
 	/**
 	 * AutoRetentionLevel: The system will automatically choose how often to run an automatic Retention Pass after each backup job.
